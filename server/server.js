522405a568fc6fe75781cd6818d762cd1d8062d3
Handana <handanawilli0902@gmail.com>
2020-08-23 13:49:32 +0700

    Attempt kanban integration

--- server/server.js
+++ server/server.js
@@ -1,9 +1,9 @@
 const http = require('./bin/http')
 const createRoom = require('./helpers/rooms')
-// const server = require('http').createServer(app)
+const server = require('http').createServer()
 const io = require('socket.io')(http)
 const axios = require('axios')
-// const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 3001
 let rooms = createRoom()
 
 
